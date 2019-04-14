be3cb25e2ac723a13e81aef2ee676a8c820897fa
YGingko <haitaoh@outlook.com>
2019-04-14 23:28:58 +0800

    create ignore file

---
+++ Demo/LM Neural Network/lm_model.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 13 21:41:53 2019
+
+@author: haitao
+"""
+
+import pandas as pd
+
+df = pd.read_excel('model.xls')
--- HTools/Test.py
+++ HTools/Test.py
@@ -5,3 +5,8 @@ Created on Sat Apr 13 11:21:33 2019
 @author: haitao
 """
 
+import pandas as pd
+df = pd.read_excel('Test Data/missing_data.xls', header=None)
+
+import Interpolate as inp
+df = inp.lagrange_df(df)
