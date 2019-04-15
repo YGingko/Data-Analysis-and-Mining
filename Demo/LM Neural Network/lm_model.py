5c9000efa375d6c58f8049d19cde012cb4ba94e7
YGingko <ygingko@qq.com>
2019-04-15 18:25:17 +0800

    update lm model

--- Demo/LM Neural Network/lm_model.py
+++ Demo/LM Neural Network/lm_model.py
@@ -8,3 +8,13 @@ Created on Sat Apr 13 21:41:53 2019
 import pandas as pd
 
 df = pd.read_excel('model.xls')
+
+from sklearn.model_selection import train_test_split
+
+train, test = train_test_split(df, test_size=0.3, random_state=0)
+
+
+from keras.models import Sequential # 神经网络初始化函数
+from keras.layers.core import Dense, Activation # 神经网络层函数、激活函数
+
+netfile = 'net.model'
